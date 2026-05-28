b3f2e3f896fa8de6463eecbf7cea6f0180dceced
Anastasiia Bakhmutova <bakhmutovaaa@gmail.com>
2026-05-28 09:38:33 +0200

    feat(OE Controller): implement file validation and extraction logic in ParseSummary

--- OrderModule.Web/Controllers/OrderExtractorController.cs
+++ OrderModule.Web/Controllers/OrderExtractorController.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using OrderModule.Web.Models;
-
-
+using OrderModule.Application.OrderExtractorService;
+using OrderModule.Application.OrderExtractorService.Models;
+using OrderModule.Application.OrderExtractorService.Utils;
 
 namespace OrderModule.Web.Controllers
 {
     public class OrderExtractorController : Controller
     {
+        
+        private readonly MessageProcessor _messageProcessor;
+        
+        public OrderExtractorController(
+            MessageProcessor messageProcessor
+        )
+        {
+            _messageProcessor = messageProcessor;
+        }
+        
         public IActionResult Index()
         {
             return View(new ViewModelSummary());
@@ -15,11 +26,24 @@ namespace OrderModule.Web.Controllers
 
         // Working with Summary form
         [HttpPost]
-        public virtual async Task<IActionResult> ParseSummary(IFormFile file)
+        public async Task<IActionResult> ParseSummary(IFormFile file)
         {
+            if (file == null)
+                return BadRequest("File wasn't provided");
+
+            using var stream = file.OpenReadStream();
+            ExtractedSummary summary = await _messageProcessor.ProcessMessageAsync(stream, file.FileName);
+
             ViewModelSummary viewModel = new ViewModelSummary()
             {
-                
+                Invoice = summary.Invoice,
+                DepDate = summary.DepDate,
+                DepPoint = summary.DepPoint,
+                ArrDate = summary.ArrDate,
+                ArrPoint = summary.ArrPoint,
+                Transport = summary.Transport,
+                Products = summary.Products,
+                Notes = summary.Notes,
             };
             return Ok(viewModel);
         }
